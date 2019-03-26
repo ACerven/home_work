0b17f485992c68553921cb8f49b77d13fac2a075
Alexandra <chervenalexandra@gmail.com>
2019-03-26 09:37:00 +0200

    XML scanner DOM system finish version

---
+++ src/main/java/scanner/dom/EmployeeXmlSearch.java
@@ -0,0 +1,39 @@
+package scanner.dom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class EmployeeXmlSearch {
+	public static void findEmloyee(String id) throws Exception {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(XmlScanner.file);
+
+		NodeList nList = doc.getElementsByTagName("employee");
+
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+
+			Node nNode = nList.item(temp);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) nNode;
+
+				if(id.equals( eElement.getAttribute("id"))) {
+					System.out.println("Staff id : " + eElement.getAttribute("id"));
+					System.out.println("First Name : " + eElement.getElementsByTagName("ferstName").item(0).getTextContent());
+					System.out.println("Last Name : " + eElement.getElementsByTagName("lastName").item(0).getTextContent());
+					System.out.println("Manager Id : " + eElement.getElementsByTagName("managerId").item(0).getTextContent());
+					System.out.println("");
+					if(!eElement.getElementsByTagName("managerId").item(0).getTextContent().equals("0"))
+					findEmloyee(eElement.getElementsByTagName("managerId").item(0).getTextContent());
+				}
+			}
+		}
+	}
+}
--- src/main/java/scanner/dom/XmlScanner.java
+++ src/main/java/scanner/dom/XmlScanner.java
@@ -12,13 +12,19 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
+//TODO add comments
+// перенести создание департаментов в отдельный метод
 public class XmlScanner {
-	private static final String file = "C:/csv/XmlDoc1.xml";
+	public static final String file = "XmlDoc1.xml";
+
 	public static void main(String[] args) throws Exception {
 		createXmlDoc();
-
+		//EmployeeXmlSearch.findEmloyee("120");
+		EmployeeXmlSearch.findEmloyee("121");
+	//	EmployeeXmlSearch.findEmloyee("122");
 	}
-	public static void createXmlDoc() throws Exception{
+
+	public static void createXmlDoc() throws Exception {
 
 		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
 
@@ -33,32 +39,32 @@ public class XmlScanner {
 
 		root.appendChild(department1);
 
-		department1.appendChild(createEmpl(document,"120","Samillin","Ignat"
-			,"19.02.1991",
+		department1.appendChild(createEmpl(document, "120", "Samillin", "Ignat"
+				, "19.02.1991",
 				"Developer"
-				,new String[]{"Work for food"}, "0"));
+				, new String[]{"Work for food"}, "0"));
 
-		department1.appendChild(createEmpl(document,"121","Kukuskin","Valera","01.04.1990","Tester"
-				,new String[]{"Work for food","Sleep 2 hour"}, "120"));
+		department1.appendChild(createEmpl(document, "121", "Kukuskin", "Valera", "01.04.1990", "Tester"
+				, new String[]{"Work for food", "Sleep 2 hour"}, "120"));
 
-		department1.appendChild(createEmpl(document,"122","Cartoha","Emilia","05.12.1995","Tester"
-				,new String[]{"Work for csrtoha", "do not need to sleep"},
+		department1.appendChild(createEmpl(document, "122", "Cartoha", "Emilia", "05.12.1995", "Tester"
+				, new String[]{"Work for csrtoha", "do not need to sleep"},
 				"120"));
 
 		Element department2 = document.createElement("department");
 
 		root.appendChild(department2);
 
-		department2.appendChild(createEmpl(document,"200","Sirotkin","Carnel"
-				,"10.12.1993",
+		department2.appendChild(createEmpl(document, "200", "Sirotkin", "Carnel"
+				, "10.12.1993",
 				"Project meneger"
-				,new String[]{"I like to work"}, "0"));
+				, new String[]{"I like to work"}, "0"));
 
-		department2.appendChild(createEmpl(document,"201","Kalisnik","Ivan","11.03.1992","Tester"
-				,new String[]{"I can sing for you"}, "200"));
+		department2.appendChild(createEmpl(document, "201", "Kalisnik", "Ivan", "11.03.1992", "Tester"
+				, new String[]{"I can sing for you"}, "200"));
 
-		department2.appendChild(createEmpl(document,"202","Joli","Anjela","06.10.1992","Tester"
-				,new String[]{"Do not need to sleep"},
+		department2.appendChild(createEmpl(document, "202", "Joli", "Anjela", "06.10.1992", "Tester"
+				, new String[]{"Do not need to sleep"},
 				"200"));
 
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -69,35 +75,35 @@ public class XmlScanner {
 	}
 
 
-	public static Element createEmpl( Document document, String id,
-									  String lname,
+	public static Element createEmpl(Document document, String id,
+									 String lname,
 									 String fname,
-							 String birth, String position, String[] skills,
-							 String managerId) {
+									 String birth, String position, String[] skills,
+									 String managerId) {
 		Element empl = document.createElement("employee");
 		Attr attr = document.createAttribute("id");
 		attr.setValue(id);
 		empl.setAttributeNode(attr);
 
-		addTextChild(document,"lastName", lname, empl);
-		addTextChild(document,"ferstName", fname, empl);
-		addTextChild(document,"birthDate", birth, empl);
-		addTextChild(document,"position", position, empl);
-		addTextChild(document,"managerId", managerId, empl);
+		addTextChild(document, "lastName", lname, empl);
+		addTextChild(document, "ferstName", fname, empl);
+		addTextChild(document, "birthDate", birth, empl);
+		addTextChild(document, "position", position, empl);
+		addTextChild(document, "managerId", managerId, empl);
 
 		Element skillsE = document.createElement("skills");
 		empl.appendChild(skillsE);
 
-		for (int i = 0; i <skills.length ; i++) {
-			addTextChild(document,"skill", skills[i],skillsE);
+		for (int i = 0; i < skills.length; i++) {
+			addTextChild(document, "skill", skills[i], skillsE);
 		}
 		return empl;
 
 	}
 
-	public static void  addTextChild(Document document,
-										   String elementName,
-									   String elementValue, Element el){
+	public static void addTextChild(Document document,
+									String elementName,
+									String elementValue, Element el) {
 		Element firstName = document.createElement(elementName);
 		firstName.appendChild(document.createTextNode(elementValue));
 		el.appendChild(firstName);
